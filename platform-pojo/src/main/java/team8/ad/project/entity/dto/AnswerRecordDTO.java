982c5553655eb35d8227955c0f54fc26c3b64fbc
Alex <1329403381@qq.com>
2025-08-07 13:56:58 +0800

    back up

--- platform-pojo/src/main/java/team8/ad/project/entity/dto/AnswerRecordDTO.java
+++ platform-pojo/src/main/java/team8/ad/project/entity/dto/AnswerRecordDTO.java
@@ -4,7 +4,10 @@ import lombok.Data;
 
 @Data
 public class AnswerRecordDTO {
+
     private Integer id; // 题目 ID
+
     private Integer correct; // 0 表示错，1 表示对
+
     private Integer param; // 学生提交的答案
 }
